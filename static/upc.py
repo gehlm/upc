61eb2838932809347947b7e76e5d70d5d6270c42
gehlm <mjgehl@gmail.com>
2016-05-07 17:48:19 -0500

    First commit

---
+++ static/upc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+# Import methods from the 21 Bitcoin Library
+from two1.commands.config import Config
+from two1.wallet import Wallet
+from two1.bitrequests import BitTransferRequests
+
+SERVER_IP_ADDRESS='10.244.69.94'
+SERVER_PORT = '5051'
+
+# Configure your Bitcoin Wallet
+username = Config().username
+wallet = Wallet()
+requests = BitTransferRequests(wallet, username)
+
+
+def get_upc(upc_code):	
+	upc_url = 'http://%s:%s/lookup?upc=%s' % (SERVER_IP_ADDRESS, SERVER_PORT, upc_code)
+	response = requests.get(url=upc_url)
+	print(response.text)
+
+
+if __name__ == '__main__':
+
+	if len(sys.argv) != 2:
+		print('Usage: python3 upc.py [UPC_CODE]')
+		sys.exit(1)
+
+	upc = sys.argv[1]
+	get_upc(upc)
---
+++ upc-server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import logging
+import yaml
+import json
+
+from flask import Flask
+from flask import request
+from flask import send_from_directory
+
+# Import from the 21 Bitcoin Developer Library
+from two1.wallet import Wallet
+from two1.bitserv.flask import Payment
+
+# See: github.com/salsa-system/weather21
+# Set up logging
+log = logging.getLogger('werkzeug')
+logging.basicConfig(filename='upc.log', level=logging.DEBUG)
+#log.setLevel(logging.ERROR)
+
+# Configure app and wallet
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+SERVER_PORT = '5051'
+UPC_URL = 'https://api.upcitemdb.com/prod/trial/lookup'
+
+
+@app.route('/manifest')
+def manifest():
+	"""Provide the app manifest to the 21 crawler.
+	"""
+	with open('./manifest.yaml', 'r') as f:
+		manifest = yaml.load(f)
+	return json.dumps(manifest)
+
+
+@app.route('/upc/client')
+def client():
+	"""Provide an example client script.
+	"""
+	return send_from_directory('static', 'upc.py')
+
+
+@app.route('/lookup')
+@payment.required(2000)
+def upc_lookup():
+	"""Charge a fixed fee per request to the UPC endpoint.
+	"""
+	upc_code = str(request.args.get('upc'))
+	userdata = {'upc': upc_code}
+	response = requests.get(UPC_URL, params=userdata)
+	return(response.text)
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=SERVER_PORT)
+
+
